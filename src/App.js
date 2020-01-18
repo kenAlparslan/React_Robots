3c1682a74e03579fffc9144d731bdb0a61bf2d06
Ken <ken@alparslan@gmail.com>
2020-01-17 19:52:56 -0500

    added the scroll functionality

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import './App.css';
+import Scroll from './Scroll.js';
 
 class App extends Component {
   constructor() {
@@ -36,7 +37,9 @@ class App extends Component {
         <div className='tc'>
           <h1 className='f2'>React Robots</h1>
           <SearchBox searchChange={this.onSearchChange} />
-          <CardList robots={filteredRobots}/>
+          <Scroll>
+            <CardList robots={filteredRobots}/>
+          </Scroll>
         </div>
         );
       }
