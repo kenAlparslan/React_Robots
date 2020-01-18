005233cdab99ff114c608e506c489f60f7289897
Ken <ken@alparslan@gmail.com>
2020-01-18 15:00:02 -0500

    Updates

---
+++ precache-manifest.a2e574e0bdd9bb58a555ed17b8d4feba.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5c52f9cff979d2747c3c3b4d6fd089a",
+    "url": "/React_Robots/index.html"
+  },
+  {
+    "revision": "6188208f596b6812ff53",
+    "url": "/React_Robots/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "3e9be6a2e80546caf2c4",
+    "url": "/React_Robots/static/css/main.a124242f.chunk.css"
+  },
+  {
+    "revision": "6188208f596b6812ff53",
+    "url": "/React_Robots/static/js/2.4b9de80c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/React_Robots/static/js/2.4b9de80c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3e9be6a2e80546caf2c4",
+    "url": "/React_Robots/static/js/main.0e4d5d9a.chunk.js"
+  },
+  {
+    "revision": "48c7d71b8fea2c78645e",
+    "url": "/React_Robots/static/js/runtime-main.ecf6abc9.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/React_Robots/static/media/SEGA.12a3db8b.woff"
+  }
+]);
